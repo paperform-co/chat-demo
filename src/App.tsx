49ad0b71a81ad57babae4d15482c104a38e03a14
Josh Cuneo <josh@paperform.co>
2024-04-30 13:38:11 +1000

    setup react app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="h-svh w-svh flex items-center justify-center bg-gray-800">
+      <div className="text-gray-50 text-4xl font-bold">Hello!</div>
+    </main>
+  );
+}
+
+export default App;
